ed37a8a62cf7045034ee63f287fa20d41c1a7dfc
Soohwan-hub <Soohwankim15@gmail.com>
2025-09-28 14:58:31 -0500

    Inital Project Setupt and MVVM Structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # VIN-Scout
+		
---
+++ VINScout/VINScout/VINScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  VINScoutApp.swift
+//  VINScout
+//
+//  Created by user283826 on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct VINScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
